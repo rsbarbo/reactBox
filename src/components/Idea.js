58ae70da86052e42b18cbe1047c7cbc34d9b9114
Raphael Barbo <rsbarbo@gmail.com>
2017-01-04 11:45:06 -0700

    update idea has been included into Idea component

--- src/components/Idea.js
+++ src/components/Idea.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Idea = ({ title, body, id, destroy }) => {
+const Idea = ({ title, body, id, destroy, update }) => {
   return(
     <div className="Idea">
       <h3>{title}</h3>
       <p>{body}</p>
       <button onClick={ () => destroy(id) }>Destroy</button>
-      <button>Update</button>
+      <button onClick={ () => update(id) }>Update</button>
     </div>
   )
 }
