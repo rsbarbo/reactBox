f46f0ab4414b29e5ca8b592d786a4c04bdfc363b
Raphael Barbo <rsbarbo@gmail.com>
2017-01-04 10:38:39 -0700

    update IdeaList component with const Idea

--- src/components/IdeaList.js
+++ src/components/IdeaList.js
@@ -4,7 +4,18 @@ const IdeaList = ({ ideas }) => {
   return(
     <div className='IdeaList'>
       // I need to render all the ideas and iterate through them
+      { ideas.map( idea => <Idea { ...idea } /> ) }
+    </div>
+  )
+}
 
+const Idea = ({ title, body, id }) => {
+  return(
+    <div className="Idea">
+      <h3>{title}</h3>
+      <p>{body}</p>
+      <button>Destroy</button>
+      <button>Update</button>
     </div>
   )
 }
