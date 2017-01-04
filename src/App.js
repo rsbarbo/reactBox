e1ee1305b42fe71c25a191d8b22c8a2490747083
Raphael Barbo <rsbarbo@gmail.com>
2017-01-04 10:21:03 -0700

    update CreateIdea component to save idea, and update App to hold and render ideas

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import CreateIdeaForm from './components/CreateIdeaForm.js'
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = { ideas: [] }
+  }
+
+  storeIdea(idea){
+    //peform the action of adding our new idae to all the ideas
+    this.state.ideas.push( idea )
+    let ideas = this.state.ideas
+
+    this.setState({ ideas: ideas })
+  }
+
   render() {
     return (
-      <CreateIdeaForm/>
+      <CreateIdeaForm saveIdea={ this.storeIdea.bind(this) }/>
     );
   }
 }
--- src/components/CreateIdeaForm.js
+++ src/components/CreateIdeaForm.js
@@ -15,16 +15,30 @@ class CreateIdeaForm extends React.Component {
     this.setState({ [fieldName]: fieldValue })
   }
 
+  createIdea(event){
+    event.preventDefault();
+
+    const idea = { title: this.state.title, body: this.state.body, id: Date.now() }
+
+    this.props.saveIdea( idea )
+
+    this.setState({ title: '', body: '' })
+  }
+
   render(){
     return(
       <div>
         <input className='CreateIdea-title'
                type='text' name='title'
                placeholder='Title'
+               value={ this.state.title }
                onChange={ (event) => this.updateProperties(event) }/>
         <textarea className="CreateIdea-body"
-                  name='body' placeholder='Body'/>
-        <button className='CreateIdea-submit'>Submit</button>
+                  name='body' placeholder='Message...'
+                  onChange={ (event) => this.updateProperties(event) }
+                  value={ this.state.body }/>
+        <button className='CreateIdea-submit'
+                onClick={ (event) => this.createIdea(event) }>Submit</button>
       </div>
     )
   }
